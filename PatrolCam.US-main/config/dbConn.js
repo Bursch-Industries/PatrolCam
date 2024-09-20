9e720ee84754a1e1cac7162c857d01d4e823f247
Skeeter Cathcart <skeetercathcart@gmail.com>
2024-09-20 13:11:15 -0500

    MongoDB Connection added

--- PatrolCam.US-main/config/dbConn.js
+++ PatrolCam.US-main/config/dbConn.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 
 const connectDB = async () => {
 
     try {
-        await mongoose.connect(process.env.DATABASE_URI);
+        await mongoose.connect(process.env.MONGODB_DATABASE_URL);
     } catch (err) {
         console.error(err);
     }
 }
 
 module.exports = connectDB;
+
--- PatrolCam.US-main/server.js
+++ PatrolCam.US-main/server.js
@@ -10,6 +10,15 @@ const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB
+connectDB();
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// json handling middleware
+app.use(express.json());
+
 // Middleware to handle url encoded data (form data)
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,6 +28,7 @@ app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // Routers
 app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/api/register'));
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
