f00ce485f07d23fdda70cbbedec25d9ddb9178f4
Skeeter Cathcart <skeetercathcart@gmail.com>
2024-10-29 11:17:13 -0500

    Remember Password now stores password in localStorage. **THIS IS NOT SECURE**. Will need to change exact implementation.

--- PatrolCam.US-main/FrontEndJavaScript/loginSubmission.js
+++ PatrolCam.US-main/FrontEndJavaScript/loginSubmission.js
@@ -7,12 +7,18 @@ const toggleVisible = (event) => {
     }
 }
 
-
 const passwordInput = document.getElementById('password');
 const toggleButton = document.getElementById('toggleButton');
-toggleButton.addEventListener('click', toggleVisible);
+const rememberMe = document.getElementById('remember');
+//toggleButton.addEventListener('click', toggleVisible);
 
-
+window.onload = function() {
+    const savedPassword = localStorage.getItem('password');
+    if(savedPassword){
+        document.getElementById('password').value = savedPassword;
+        document.getElementById('remember').checked = true;
+    }
+}
 
 
 document.getElementById('loginForm').addEventListener('submit', async function(event) {
@@ -21,9 +27,14 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
 
+
     // Get the values from the input fields
     const username = usernameInput.value;
     const password = passwordInput.value;
+
+
+    console.log('username: ' + username);
+    console.log('password: ' + password);
 
     // Clear previous error messages
     //errorMessage.textContent = '';
@@ -40,14 +51,15 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
 
 
     // Check password regex (8 characters, one upper, one lower, one special)
-    if (password != "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$") {
+    /* if (password != "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$") {
         usernameInput.style.border = '2px solid red';
         passwordInput.style.border = '2px solid red';
         console.log('password must contain one uppercase, one lowercase, one special character, etc...');
         return;
-    }
+    } */
 
     try {
+        console.log('entering try block')
         // If username and password exist, POST to server API
         const response = await fetch('/login/login', {
             method: 'POST',
@@ -69,7 +81,13 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
 
             return; // Stop further execution
         }
-
+        console.log('rememberMe: ' + rememberMe);
+        if(document.getElementById('remember').checked) {
+            localStorage.setItem('password', password)
+        } else {
+            localStorage.removeItem('password')
+        }
+        
         console.log('Login successful:');
         window.location.href = '/protected'; // Adjust the URL as necessary
         
