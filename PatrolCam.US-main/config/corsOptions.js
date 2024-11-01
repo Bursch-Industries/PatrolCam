6df21caf05dabcc5841cdff62f377163f77b1612
Skeeter Cathcart <skeetercathcart@gmail.com>
2024-11-01 15:48:17 -0500

    cors

--- PatrolCam.US-main/config/corsOptions.js
+++ PatrolCam.US-main/config/corsOptions.js
@@ -1,14 +1,15 @@
-const whitelist = ['https://www.patrolcam.com', 'http://agile-crow-usoj23gwgmlexirrdv42lwyh.herokudns.com', 'http://patrolcam.us', 'http://localhost:3000']; // A list of domains that are ALLOWED to access backend API
+const whitelist = ['https://www.patrolcam.com', 'http://agile-crow-usoj23gwgmlexirrdv42lwyh.herokudns.com/', 'http://patrolcam.us', 'http://localhost:3000']; // A list of domains that are ALLOWED to access backend API
 // Checks for domains in the above list. 
 const corsOptions = {  
     origin: (origin, callback) => {
-        if (whitelist.some(allowedOrigin => origin.startsWith(allowedOrigin))) {
-            callback(null, true);
-        } else {
-            callback(new Error('Error: Not allowed by CORS'));
+        if (whitelist.indexOf(origin) !== -1) { 
+            callback(null, true)
+        } 
+        else {
+            callback(new Error ('Error: Not allowed by CORS'))
         } 
     },
     optionsSuccessStatus: 200
-};
+}
 
 module.exports = corsOptions
