3b853ef807e26fa8209fda71974d2f5a36b4b130
Ethan Omwoyo <123187384+Ethanomwoyo@users.noreply.github.com>
2025-03-06 00:42:49 -0600

    Start of landing page

--- PatrolCam.US-main/next-app/next-app/src/app/page.jsx
+++ PatrolCam.US-main/next-app/next-app/src/app/page.jsx
@@ -4,7 +4,22 @@ import '@/app/styles/globals.css';
 export default function LandingPage() {
   return (  
     <div>
-      <h1>Patrol Cam</h1>
+      {/* First part of the website that users see */}
+      <div>
+        <h1>
+          The Most Advanced<br/>
+          Surveillance<br/>
+          Solutions For<br/>
+          Defense Agencies
+        </h1>
+        <button>Get Started</button>
+        <div>Logo</div>
+      </div>
+      
+      {/* Start of the features section */}
+
+      {/* Start of the contact form section */}
+
     </div>
   )
 }
