43b365aec448a0192b707bbc0b5d73de0ca031a2
Ethanomwoyo <sn5881tb@go.minnstate.edu>
2025-03-20 22:50:30 -0500

    surv. page started

---
+++ PatrolCam.US-main/next-app/next-app/src/app/dashboard/surveillance/page.jsx
@@ -0,0 +1,10 @@
+// page for the surveillance of PatrolCam
+'use client';
+
+export default function Surveillance(){
+    return (
+        <div className="bg-[#2E8BC0] h-screen">
+            <h1 className="text-center text-white ">This is the surveillance page</h1>
+        </div>
+    );
+}
