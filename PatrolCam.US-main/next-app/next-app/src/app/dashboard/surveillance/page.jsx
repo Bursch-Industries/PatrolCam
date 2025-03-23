9181a2991f4237c4ff363c0396faba9aea21bad8
Ethanomwoyo <sn5881tb@go.minnstate.edu>
2025-03-22 22:53:55 -0500

    progress

--- PatrolCam.US-main/next-app/next-app/src/app/dashboard/surveillance/page.jsx
+++ PatrolCam.US-main/next-app/next-app/src/app/dashboard/surveillance/page.jsx
@@ -16,22 +16,35 @@ function SurvCamera( { src } ){
 
 export default function Surveillance(){
 	// state for camera grids
-	const [row, setRows] = useState(1); // state var to get and set rows
-	const [col, setCols] = useState(1); // state var to get and set cols
-	let demoVid = "/Camera_1.mp4"
+	const [row, setRows] = useState(3); // state var to get and set rows
+	const [col, setCols] = useState(3); // state var to get and set cols
+	const demoCameras = {
+		camera1: "/Camera_1.mp4",
+		camera2: "/Camera_2.mp4",
+		camera3: "/Camera_3.mp4",
+		camera4: "/Camera_4.mp4",
+		camera5: "/Camera_5.mp4",
+		camera6: "/Camera_6.mp4",
+		camera7: "/Camera_7.mp4",
+		camera8: "/Camera_8.mp4",
+		camera9: "/Camera_9.mp4",
+	}
 
     return (
-        <div className="bg-[#2E8BC0] flex h-screen">
-
-            {/* grid camera container */}
-			<div className="flex w-[100%] m-8">
+        <div className="bg-[#2E8BC0] flex flex-col h-screen">
+			{/* Temporary dropdown until the navbar is fixed */}
+			<div className="flex justify-end">
+				<button className="mr-4 mt-2 bg-primary p-2 px-3 rounded-md text-white text-2xl">☰</button>
+			</div>
+			{/* grid camera container */}
+			<div className="flex w-[100%] h-[100%] m-8">
 				<div className={`grid grid-rows-${row} grid-cols-${col} w-[100%]`}> 
 					{/* insert video frames here */}
-					<SurvCamera src={demoVid} />
+					<SurvCamera src={demoCameras.camera1} />
 				</div>
 			</div>
+		</div>
 
-        </div>
     );
 }
 
