e4818075e2f84342525afca938e25edb5c812cbc
Ethanomwoyo <sn5881tb@go.minnstate.edu>
2025-03-28 13:53:13 -0500

    Start homepage design

--- PatrolCam.US-main/next-app/next-app/src/app/dashboard/page.jsx
+++ PatrolCam.US-main/next-app/next-app/src/app/dashboard/page.jsx
@@ -12,7 +12,10 @@ export default function DashboardPage() {
     return (
         <div className="base-background h-screen">
             <div className="flex justify-center gap-4">
-                <h1 className="text-white text-4xl mt-4">Dashboard page {session?.user.name}</h1>  
+                <h1 className="text-white text-4xl mt-4">Dashboard page {session?.user.name}</h1>
+                <div className="flex justify-center items-center">
+                    <p className="text-white text-2xl mt-4">Insert Home page design here</p> 
+                </div>
             </div>
         </div>
     );
