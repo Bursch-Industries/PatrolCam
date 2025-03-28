98979b10a397176e2634563c2dfa52896c65f336
Ethanomwoyo <sn5881tb@go.minnstate.edu>
2025-03-27 20:28:08 -0500

    "middleware comments"

--- PatrolCam.US-main/next-app/next-app/src/middleware.js
+++ PatrolCam.US-main/next-app/next-app/src/middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
     const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
     if (!session) {
-        console.log("No session");
+        console.log("No session", session);
         // if no session exists, redirect to login
         return NextResponse.redirect(new URL("/login", req.url));
     }
@@ -15,5 +15,5 @@ export async function middleware(req) {
 }
 
 export const config = {
-    matcher: ["/dashboard/:path*"], // Apply middleware to these routes
+    matcher: ["/dashboard/:path*"], // Apply middleware to these routes TODO: ADD protected routes for different roles
 };
