c3824439d33e0de7135aa48063e4e8eb695a260e
Ethanomwoyo <sn5881tb@go.minnstate.edu>
2025-04-01 14:13:15 -0500

    this is almost done trust me

---
+++ PatrolCam.US-main/next-app/next-app/src/app/api/auth/emailAPI/route.js
@@ -0,0 +1,83 @@
+const nodemailer = require('nodemailer');
+
+
+export async function POST(req){
+    // get the form info from the request
+    const formInfo = await req.json();
+    const { contactName, organization, phoneNumber, email, productInterest, extension } = formInfo;
+
+    if (!contactName || !email || !organization || !phoneNumber || !productInterest) {
+        return new Response(
+            JSON.stringify({error: 'failed to process request'}),
+            { status: 400 }
+        )
+    }
+
+    try {
+        // Creat nodemailer transport (connection to SMTP server)
+        const transporter = nodemailer.createTransport({
+            service: 'gmail', 
+            auth: {
+                user: 'mekashaniftalem@gmail.com', // REPLACE with email address that is being "charged" for the emails
+                pass: process.env.GMAIL_APP // REPLACE with app password corresponding to above email
+            }
+        });
+
+        // Initialize mailOptions as empty, as there will be two potential formats
+        let mailOptions;
+
+        // If the user did NOT provide an extension, format the email without it
+        if (extension === 'optional') {
+            mailOptions = {
+                from: `"${contactName}" <${email}>`,
+                to: 'patrolcamproject@gmail.com', 
+                subject: 'New Contact Form Submission',
+                text: `New inquiry from: \n ${contactName} at ${organization} \n Phone: ${phoneNumber} \nAbout: ${productInterest}`,
+        };
+
+        // Format for contact form with an extension
+        } else {
+            mailOptions = {
+                from: `"${contactName}" <${email}>`,
+                to: 'patrolcamproject@gmail.com', 
+                subject: 'New Contact Form Submission',
+                text: `New inquiry from: \n${contactName} at ${organization} \nPhone: ${extension} - ${phoneNumber} \nAbout: ${productInterest}`,
+            };
+        }
+
+        // Send the email
+        await transporter.sendMail(mailOptions);
+
+        // // Log email sent action
+        // await withTransaction(async (session) => {
+            
+        //     await logActivity({
+        //         action: 'Contact Form Submission',
+        //         session
+        //     });
+        // });
+        
+        return new Response (
+            { status: 200 }
+        )
+        
+    } catch (error) {
+        // await withTransaction(async (session) => {
+                
+        //     await logError(req, {
+        //         level: 'ERROR',
+        //         desc: 'Failed to send email',
+        //         source: 'emailController',
+        //         userId: '',
+        //         code: '500',
+        //         meta: { message: err.message, stack: err.stack },
+        //         session
+        //     });
+
+        // });
+
+        return new Response(
+            { status: 500 }
+        )
+    }
+}
--- PatrolCam.US-main/next-app/next-app/src/components/contactUs.jsx
+++ PatrolCam.US-main/next-app/next-app/src/components/contactUs.jsx
@@ -2,53 +2,147 @@
 'use client'; 
 import { useState } from "react";
 
-
 export default function ContactForm() {
     // state for form data that will be sent to the backend
-    const [formData, setFormData] = useState({
+    const [formInfo, setFormInfo] = useState({
         contactName: (''),
         organization: (''),
         phoneNumber : 0,
         email: (''),
         productInterest: (''),
+        extension: ('optional'),
     });
 
 
-    // make async function
-    function HandleSubmission(e){
-        e.preventDefault();
-    }
-
+    
     function HandleName(e){
-        setFormData( prev => ({
+        setFormInfo( prev => ({
             ...prev,
             contactName: e.target.value,
         }));
     }
-
+    
     function HandleOrg(e){
-        setFormData( prev => ({
+        setFormInfo( prev => ({
             ...prev,
             organization: e.target.value,
         }));
     }
+    
+    function EnforceFormat(e){
+        // Allows numeric input in phone number field
+        
+        const isNumericInput = (e) => {
+            const key = e.keyCode;
+            return ((key >= 48 && key <= 57) || // Allow number line
+            (key >= 96 && key <= 105) // Allow number pad
+        );
+    };
+    
+    // Allows modifier keys to be used in phone number field
+    const isModifierKey = (e) => {
+        const key = e.keyCode;
+        return (e.shiftKey === true || key === 35 || key === 36) || // Allow Shift, Home, End
+        (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
+        (key > 36 && key < 41) || // Allow left, up, right, down
+        (
+            // Allow Ctrl/Command + A,C,V,X,Z
+            (e.ctrlKey === true || e.metaKey === true) &&
+            (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
+        )
+    };
+    
+    // Combines the allowed keys into one event
+    // Input must be of a valid number format or a modifier key, and not longer than ten digits
+    if(!isNumericInput(e) && !isModifierKey(e)){
+        e.preventDefault();
+    }
+    
+    // Formats the input into desired structure
+    if(isModifierKey(e)) {return;}
+    
+    const input = e.target.value.replace(/\D/g,'').substring(0,10); // First ten digits of input only
+    const areaCode = input.substring(0,3);
+    const middle = input.substring(3,6);
+    const last = input.substring(6,10);
+    
+    if(input.length > 6){e.target.value = `(${areaCode}) - ${middle} - ${last}`;}
+    else if(input.length > 3){e.target.value = `(${areaCode}) - ${middle}`;}
+    else if(input.length > 0){e.target.value = `(${areaCode}`;}
+    
+}
 
     function HandlePhoneNumber(e){
-        // logic for handling formatting and state of user phone number
+        setFormInfo(prev => ({
+            ...prev,
+            phoneNumber: e.target.value,
+        }))
+    }
+
+    function HandleExtension(e) {
+        setFormInfo(prev => ({
+            ... prev,
+            extension: e.target.value,
+        }))
     }
 
     function HandleEmail(e){
-        // logic for handling state of inputted email
+        setFormInfo(prev => ({
+            ...prev,
+            email: e.target.value,
+        }))
     }
 
     function HandleProductInterest(e){
         // logic for handling product interest selection
+        setFormInfo( prev => ({
+            ...prev,
+            productInterest: e.target.value,
+        }))
     }
 
 
-    
+    // make async function
+    async function HandleFormSubmission(e){
+        e.preventDefault(); // prevent the default submission
 
-    return (
+        let isInvalid = false;
+
+        Object.entries(formInfo).forEach( (key, value) => {
+            if (String(value).trim() === '' || 0){
+                isInvalid = true;
+                console.log('form contains empty input field');
+                // TODO: add red border to invalid input field
+            } else {
+                console.log('form is good for submission')
+                // TODO: add green border to all valid input fields or show success
+            }
+        });
+
+        if (isInvalid){
+            return; // form submission has invalid properties, stop execution
+        } else {
+            try {
+                const requestBody = formInfo;
+                const response = await fetch('/api/auth/emailAPI', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(requestBody),
+                });
+                if (!response.ok){
+                    console.log('An error has occured');
+                    return; // problem with response, stop execution
+                }
+            } catch (error) {
+                console.error('Error: ', error);
+            }
+        }
+    }
+
+
+return (
         // contact section container
         <section id="contact-us" className="flex bg-[#145DA0] justify-center">
             {/* form container */}
@@ -58,7 +152,7 @@ export default function ContactForm() {
                 </h1>
                 <p className="text-primary text-center text-[1.5625rem] ">Let us know what you are interested in</p>
 
-                <form onSubmit={HandleSubmission}>
+                <form onSubmit={HandleFormSubmission}>
                     <input
                         type="text"
                         placeholder="Contact Name"
@@ -74,27 +168,31 @@ export default function ContactForm() {
                     <div className="flex justify-between mt-4">
                         <input
                             type="text"
-                            placeholder="Extension (optional)"
-                            className="bg-white rounded-md pl-2 py-2 w-[50%] mr-1 shadow-lg"
+                            placeholder="Ext. (optional)"
+                            onChange={HandleExtension}
+                            className="bg-white rounded-md pl-2 py-2 w-[30%] mr-1 shadow-lg"
                         />
                         <input 
                             type="text"
                             placeholder="Phone Number"
+                            onKeyDown={EnforceFormat}
+                            onChange={HandlePhoneNumber}
                             maxLength="18"
-                            className="bg-white rounded-md pl-2 py-2 w-[50%] ml-1 shadow-lg"
+                            className="bg-white rounded-md pl-2 py-2 w-[70%] ml-1 shadow-lg"
                         />
                     </div>
                     <input
                         type="email"
                         placeholder="E-mail"
+                        onChange={HandleEmail}
                         className="bg-white rounded-md w-[100%] pl-2 py-2 mt-4 mb-4 shadow-lg"
                     />
                     <label htmlFor="productInterest" className="block text-center text-2xl "> What product are you interested in? </label>
-                    <select className="bg-white rounded-md pl-2 py-2 mt-1 w-[100%] shadow-lg">
-                        <option value="option-1"> Surveillance Cameras</option>
-                        <option value="option-2"> Option 2</option>
-                        <option value="option-3"> Option 3</option>
-                        <option value="option-4"> Other</option>
+                    <select onChange={HandleProductInterest} className="bg-white rounded-md pl-2 py-2 mt-1 w-[100%] shadow-lg">
+                        <option value="Surveillance Cameras"> Surveillance Cameras</option>
+                        <option value="Option-2"> Option 2</option>
+                        <option value="Option-3"> Option 3</option>
+                        <option value="Other"> Other</option>
                     </select>
                     <div className="flex justify-center mt-4">
                         <button type="submit" className="bg-primary text-white text-lg px-8 py-2 rounded-md hover:bg-blue-900 shadow-lg transition-normal">Submit</button>
